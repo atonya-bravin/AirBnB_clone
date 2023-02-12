16f908026a616ff60a33064668d2e81b9e3c8563
bravin-atonya <bravinatonya.brani@gmail.com>
2023-02-11 18:54:33 -0800

    models/base_model.py: all task 3 tasks

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import uuid
+import datetime
+
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+class BaseModel():
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        time_dict = self.__dict__.copy()
+        if "created_at" in time_dict:
+            time_dict["created_at"] = time_dict["created_at"].strftime(time_format)
+        if "updated_at" in time_dict:
+            time_dict["updated_at"] = time_dict["updated_at"].strftime(time_format)
+        return (time_dict)
